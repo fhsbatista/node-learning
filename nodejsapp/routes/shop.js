c63d7c8f5968067baa83a1298edb0aeb09dadca8
Fernando Batista <fernandohsbatista@gmail.com>
2021-09-26 19:01:08 -0300

    Section 5 - 70. Using path to return html views

--- nodejsapp/routes/shop.js
+++ nodejsapp/routes/shop.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello to express</h1>');
+    // "__dirname" => the path of this file (shop.js) on the operation system
+    // "../" is for going one folder back so that we can find the "views" folder
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
