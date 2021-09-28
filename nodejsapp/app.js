e1fd42a3c2510458cc4c7ffce7539a2859473071
batista <fernandohsbatista@gmail.com>
2021-09-27 22:21:16 -0300

    Section 5 - 71. Using path to return a 404 html page

--- nodejsapp/app.js
+++ nodejsapp/app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const routes = require('./routes');
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
