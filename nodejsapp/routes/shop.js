330a17f1d8fc9455bb9bec173521a0eb15d9a29c
batista <fernandohsbatista@gmail.com>
2021-09-28 20:04:19 -0300

    Section 5 - 73. Created helper method to get the root directory path

--- nodejsapp/routes/shop.js
+++ nodejsapp/routes/shop.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require('../utils/path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     // "__dirname" => the path of this file (shop.js) on the operation system
     // "../" is for going one folder back so that we can find the "views" folder
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
