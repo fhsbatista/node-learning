37b5bd1461610b00e643c97b61058ad5e63823e7
batista <fernandohsbatista@gmail.com>
2021-10-06 20:51:21 -0300

    Section 6 - 79. Exposing values through module exports

--- nodejsapp/routes/shop.js
+++ nodejsapp/routes/shop.js
@@ -3,7 +3,10 @@ const path = require('path');
 const rootDir = require('../utils/path');
 const router = express.Router();
 
+const adminData = require('./admin');
+
 router.get('/', (req, res, next) => {
+    console.log(adminData.products);
     // "__dirname" => the path of this file (shop.js) on the operation system
     // "../" is for going one folder back so that we can find the "views" folder
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
