a7a83f7cdf27269b6036ba1b5418c25a4aff2afe
batista <fernandohsbatista@gmail.com>
2021-10-10 20:04:09 -0300

    Section 6 - 81. Implemented pug to render shop page

--- nodejsapp/app.js
+++ nodejsapp/app.js
@@ -6,6 +6,9 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
