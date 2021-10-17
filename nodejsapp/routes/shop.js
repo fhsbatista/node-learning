71dbd0d203d259825df0433485669049f621980b
batista <fernandohsbatista@gmail.com>
2021-10-17 09:29:48 -0300

    Section 6 - 90. Implemented main layout for hbs pages

--- nodejsapp/routes/shop.js
+++ nodejsapp/routes/shop.js
@@ -7,7 +7,16 @@ const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
     const products = adminData.products;
-    res.render('shop', { products: products, pageTitle: 'Shop', path:'/', hasProducts: products.length > 0});
+    res.render(
+        'shop',
+        {
+            products: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true,
+        });
 });
 
 module.exports = router;
